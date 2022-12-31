4468bf8005df5243906ae0cc56ffbbce1d5d6ddf
Sergey Matofonov <phaeton.kz@gmail.com>
2022-12-31 14:35:13 +0300

    modules

---
+++ modules/main.py
@@ -0,0 +1,10 @@
+from application.salary import calculate_salary
+from application.db.people import get_employees
+from datetime import date
+from pong_game import
+if __name__ == '__main__':
+    calculate_salary()
+    get_employees()
+    print(date.today())
+
+    
