35b8ef9cbdea81fceac313c02e984861f59f0389
Sergey Matofonov <phaeton.kz@gmail.com>
2023-01-17 14:36:34 +0300

    Add web-scrapping

---
+++ web_scraping/main.py
@@ -0,0 +1,68 @@
+import requests
+from bs4 import BeautifulSoup
+from fake_headers import Headers
+from pprint import pprint
+import json
+
+
+HOST = "https://spb.hh.ru/search/vacancy?text=python&area=1&area=2"
+
+
+def get_headers():
+    return Headers(browser='firefox', os='win').generate()
+
+
+def main():
+    """  Данаая функция ищет на сайте hh.ru только те вакансии, у которых в описании есть ключевые слова "Django" и "Flask"
+        По умолчанию поиск идет по краткому описанию, так же можно сделать глубокий поиск по подробному описанию на детальной
+        странице вакансии, но это увеличивает время запроса значительно, потому что нужно "проваливаться" по каждой ссылке.
+    """
+    hh_main_html = requests.get(url=HOST, headers=get_headers()).text
+    if requests.codes.ok == 200:
+        soup = BeautifulSoup(hh_main_html, features='lxml')
+        # Забираем список вакансий...hh.ru через bs4 отдает первые 20 вакансий
+        vacancy_list_tag = soup.find('div', id='a11y-main-content')
+        vacancy_tag = vacancy_list_tag.find_all(class_='serp-item')
+        vacancies = []
+        for vacancy in vacancy_tag:
+            link = vacancy.find('a', class_='serp-item__title')['href']
+            # Поле с ЗП может отсутствовать, пробуем ее получить в противном случае сообщаеем что ЗП не указана
+            try:
+                salary = vacancy.find('span', class_='bloko-header-section-3').text
+            except Exception:
+                salary = 'Зарплата не указана'
+            company = vacancy.find('a', class_="bloko-link_kind-tertiary").text
+            # .bloko-text встречается несколько раз и не вложенно, забираем второй встречающийся через select
+            address = vacancy.select('.bloko-text')[1].text
+            # В поле город, так же находится через запятую и ближайшее метро, оно нам не нужно, забираем только город
+            city = address.split(",")[0]
+            # Поиск по краткому описанию на странице со списком вакансий
+            description_body = vacancy.find(class_='g-user-content').text
+            # Глубокий поиск по подробноуму описанию на детальной странице вакансии. Время поиска значительно выше...
+            # description_html = requests.get(link, headers=get_headers()).text
+            # description_body = BeautifulSoup(description_html, features='lxml').find(class_='g-user-content').text
+            if 'Django' in description_body or 'Flask' in description_body:
+                vacancies.append({
+                    'link': link,
+                    'salary': salary,
+                    'company':company,
+                    'city': city,
+                    'description': description_body
+                })
+        return vacancies
+    else:
+        print('Ошибка подключения к сайту {HOST}')
+
+
+def write_json(vac):
+    """ Записывает полученный список словарей с нужными вакансиями в json файл
+    """
+    with open('vacancies.json', mode='w', encoding='utf-8') as f:
+        json.dump(vac, f, ensure_ascii=False, indent=2)
+
+
+if __name__ == '__main__':
+    list_vacancies = main()
+    write_json(list_vacancies)
+    print('Файл "vacancies.json" с вакансиями готов!')
+
